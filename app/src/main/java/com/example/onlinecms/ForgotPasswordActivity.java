781d314044313af56ea9e9d54a8f2bdab976b565
Faruk Colak <faruk_colak53@hotmail.com>
2020-11-21 18:30:17 +0300

    forgot password ekledim.

--- app/src/main/java/com/example/onlinecms/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/onlinecms/ForgotPasswordActivity.java
@@ -1,13 +1,48 @@
 package com.example.onlinecms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    EditText editTextEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        editTextEmail = findViewById(R.id.reset_password_email);
+    }
+
+    public void resetPassword(View view) {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        String email = editTextEmail.getText().toString();
+
+        if(!email.equals("")){
+            firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(ForgotPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    else{
+                        Toast.makeText(ForgotPasswordActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else
+            editTextEmail.setError("Enter an email!");
     }
 }
