2eaec5d71a333df4d852e6eef89dd2826ad17d1b
Rahim Gün <rahimgun2@gmail.com>
2020-12-19 14:09:23 +0300

    location of complaint added

--- app/src/main/java/com/example/onlinecms/NewComplaintActivity.java
+++ app/src/main/java/com/example/onlinecms/NewComplaintActivity.java
@@ -10,23 +10,41 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.CancellationTokenSource;
+import com.google.android.gms.tasks.OnSuccessListener;
+import android.location.Location;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class NewComplaintActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private static final int PICK_IMAGE = 100;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private FusedLocationProviderClient mFusedLocationClient;
     Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_complaint);
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         imageView = findViewById(R.id.new_complaint_image_view);
 
         if(ContextCompat.checkSelfPermission(this,
@@ -74,6 +92,11 @@ public class NewComplaintActivity extends AppCompatActivity {
 
     }
 
+    public void getLoc(View view){
+        Intent intent = new Intent(this, LocationHelper.class);
+        startActivityForResult(intent, 101);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -88,6 +111,27 @@ public class NewComplaintActivity extends AppCompatActivity {
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
         }
+
+        if(requestCode == 101){
+            if(resultCode == RESULT_OK){
+                Geocoder geocoder;
+                List<Address> addresses = null;
+                geocoder = new Geocoder(this, Locale.getDefault());
+
+                Location location = (Location) data.getExtras().get("location");
+                try {
+                    addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                TextView locationText = findViewById(R.id.location);
+                locationText.setVisibility(View.VISIBLE);
+                locationText.setText(address);
+            }
+        }
+
     }
 
     public void addFromGallery(View view) {
@@ -95,5 +139,97 @@ public class NewComplaintActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         //startActivityForResult(intent, RESULT_LOAD_IMAGE);
         startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    private boolean checkLocationPermission() {
+        //check the location permissions and return true or false.
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //permissions granted
+            Toast.makeText(getApplicationContext(), "permissions granted", Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            //permissions NOT granted
+            //if permissions are NOT granted, ask for permissions
+            Toast.makeText(getApplicationContext(), "Please enable permissions", Toast.LENGTH_LONG).show();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Permissions request")
+                        .setMessage("we need your permission for location in order to show you this example")
+                        .setPositiveButton("Ok, I agree", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(NewComplaintActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        }
+    }
+
+
+    public void getLocation() {
+        Toast.makeText(getApplicationContext(), "getLocation", Toast.LENGTH_LONG).show();
+        LocationRequest locationRequest = new LocationRequest().setInterval(2000).setFastestInterval(2000).setPriority(2000);
+        CancellationTokenSource cts = new CancellationTokenSource();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //request the last location and add a listener to get the response. then update the UI.
+            mFusedLocationClient.getCurrentLocation(1000, cts.getToken()).addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    TextView locationText = findViewById(R.id.location);
+                    locationText.setVisibility(View.VISIBLE);
+                    if(location != null){
+                        locationText.setText(location.toString());
+                    }else{
+                        locationText.setText("location: IS NULL");
+                    }
+                    // Got last known location.
+//                    TextView locationText = findViewById(R.id.locationText);
+//                    if (location != null) {
+//                        locationText.setText("location: " + location.toString());
+//                    } else {
+//                        locationText.setText("location: IS NULL");
+//                    }
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "getLocation ERROR", Toast.LENGTH_LONG).show();
+            TextView locationText = findViewById(R.id.location);
+            locationText.setText("location: ERROR");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    getLocation();
+                } else {
+                    // permission denied
+//                    TextView locationText = findViewById(R.id.locationText);
+//                    locationText.setText("location: permission denied");
+
+                    new AlertDialog.Builder(NewComplaintActivity.this)
+                            .setMessage("Cannot get the location!")
+                            .setPositiveButton("OK", null)
+                            .setNegativeButton("Cancel", null)
+                            .create()
+                            .show();
+                }
+        }
     }
 }
