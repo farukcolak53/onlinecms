488bb92cd9c589b561c8254d7580ea734e08cadd
Faruk Colak <faruk_colak53@hotmail.com>
2020-12-24 23:12:49 +0300

    progressdialog

--- app/src/main/java/com/example/onlinecms/MyAppGlideModule.java
+++ app/src/main/java/com/example/onlinecms/MyAppGlideModule.java
@@ -1,7 +1,6 @@
 package com.example.onlinecms;
 
 import android.content.Context;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
